6698252d55fd74b0f9cb86c0e4e27e399e40d181
Humphrey Joshua <joshuahumphrey579@gmail.com>
2025-04-04 13:52:15 +0100

    added native wind for styling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import './global.css';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
