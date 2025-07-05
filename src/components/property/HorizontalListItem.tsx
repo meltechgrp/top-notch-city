3392f5a9b6c6e1137783cdc651abe432117332b0
Humphrey Joshua <joshuahumphrey579@gmail.com>
2025-07-05 05:44:01 +0100

    updated nearby list item

--- src/components/property/HorizontalListItem.tsx
+++ src/components/property/HorizontalListItem.tsx
@@ -1,68 +1,53 @@
-import { cn } from '@/lib/utils';
-import { formatMoney } from '@/lib/utils';
-import capitalize from 'lodash-es/capitalize';
-import { Card, Heading, Icon, Image, Pressable, Text, View } from '../ui';
-import { Bath, Bed } from 'lucide-react-native';
-import { useRouter } from 'expo-router';
-import { generateMediaUrl } from '@/lib/api';
+import { cn, FindAmenity } from "@/lib/utils";
+import { formatMoney } from "@/lib/utils";
+import capitalize from "lodash-es/capitalize";
+import { Heading, Icon, Image, Pressable, Text, View } from "../ui";
+import { Bath, Bed } from "lucide-react-native";
+import { useRouter } from "expo-router";
+import { generateMediaUrl } from "@/lib/api";
+import { PropertyTitle } from "./PropertyTitle";
 
 type Props = {
-	data: Property;
-	className?: string;
+  data: Property;
+  className?: string;
 };
 export default function HorizontalListItem(props: Props) {
-	const { data, className } = props;
-	const { media, price, id, title, amenities } = data;
-	const router = useRouter();
-	const Find = (item: string) =>
-		amenities?.find((a) => a.name == item)?.value || 0;
-	return (
-		<Pressable
-			both
-			onPress={() => {
-				router.push({
-					pathname: `/property/[propertyId]`,
-					params: {
-						propertyId: id,
-					},
-				});
-			}}>
-			<Card
-				className={cn(
-					'relative flex-row h-[110px] rounded-xl p-2 gap-4 overflow-hidden active:scale-[0.95]',
-					className
-				)}
-				style={{ borderRadius: 8 }}>
-				<Image
-					source={{ uri: generateMediaUrl(media[0]).uri }}
-					className="h-full w-32 rounded-xl"
-					alt={title}
-				/>
-				<View className=" w-[120px]">
-					<Heading size="md" className=" font-medium" numberOfLines={1}>
-						{capitalize(title)}
-					</Heading>
-					<View className=" gap-1">
-						<View className="flex-row items-center gap-1">
-							<Text className="text-sm font-medium text-gray-400">Type:</Text>
-							<Text className="text-sm font-medium text-gray-400">Duplex</Text>
-						</View>
-						<View className="flex-row gap-3">
-							<View className="flex-row items-center gap-1">
-								<Icon as={Bed} size={'sm'} className="text-primary" />
-								<Text className="text-sm">{Find('Bedroom')}</Text>
-							</View>
-							<View className="flex-row items-center gap-1">
-								<Icon as={Bath} size={'sm'} className="text-primary" />
-								<Text className=" text-sm">{Find('Bathroom')}</Text>
-							</View>
-						</View>
-						<Text size="md" className="text-medium text-typography">
-							{formatMoney(price, 'NGN', 0)}
-						</Text>
-					</View>
-				</View>
-			</Card>
-		</Pressable>
-	);
+  const { data, className } = props;
+  const { media, price, id, title, amenities } = data;
+  const router = useRouter();
+  return (
+    <Pressable
+      both
+      onPress={() => {
+        router.push({
+          pathname: `/property/[propertyId]`,
+          params: {
+            propertyId: id,
+          },
+        });
+      }}
+    >
+      <View
+        className={cn(
+          "relative flex-row h-[110px] bg-background-muted/80 rounded-xl p-2 gap-4 overflow-hidden active:scale-[0.95]",
+          className
+        )}
+        style={{ borderRadius: 8 }}
+      >
+        <Image
+          source={{ uri: generateMediaUrl(media[0]).uri }}
+          className="h-full w-32 rounded-xl"
+          alt={title}
+        />
+        <View className=" w-[120px] gap-1">
+          <PropertyTitle property={data} smallView />
+          <View className=" gap-1">
+            <Text size="md" className="text-medium text-typography">
+              {formatMoney(price, "NGN", 0)}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </Pressable>
+  );
 }
--- src/components/property/PropertyTitle.tsx
+++ src/components/property/PropertyTitle.tsx
@@ -1,32 +1,48 @@
-import { FindAmenity } from "@/lib/utils";
+import { cn, FindAmenity } from "@/lib/utils";
 import { Text, View } from "../ui";
 
 interface Props {
   property: Property;
+  smallView?: boolean;
 }
 
 export function PropertyTitle({
   property: { purpose, amenities, subcategory },
+  smallView = false,
 }: Props) {
   if (purpose == "rent") {
     return (
-      <View className="mb-2">
+      <View className="mb-2 gap-1">
         <View className="bg-gray-500/80 rounded-md self-start py-1 px-2">
-          <Text className="text-base">To Rent</Text>
+          <Text className={cn("text-base", smallView && "text-sm")}>
+            To Rent
+          </Text>
         </View>
-        <Text className="text-2xl text-white font-bold">
-          {FindAmenity("Bedroom", amenities)} Bedroom {subcategory.name}
+        <Text
+          className={cn(
+            "text-2xl text-white font-bold",
+            smallView && "text-lg"
+          )}
+        >
+          {FindAmenity("Bedroom", amenities)} Bedroom {subcategory?.name}
         </Text>
       </View>
     );
   } else if (purpose == "sell") {
     return (
-      <View className="">
+      <View className="gap-1">
         <View className="bg-gray-500/80 rounded-md self-start py-1 px-2">
-          <Text className=" text-base">For Sale</Text>
+          <Text className={cn("text-base", smallView && "text-sm")}>
+            For Sale
+          </Text>
         </View>
-        <Text className="text-2xl text-white font-bold">
-          {FindAmenity("Bedroom", amenities)} Bedroom {subcategory.name}
+        <Text
+          className={cn(
+            "text-2xl text-white font-bold",
+            smallView && "text-sm"
+          )}
+        >
+          {FindAmenity("Bedroom", amenities)} Bedroom {subcategory?.name}
         </Text>
       </View>
     );
